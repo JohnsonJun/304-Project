09c1c04dab33ecb577baca6250f6ec82a1a0a2d4
JohnsonJun <JohnsonJun@github.com>
2016-04-03 22:16:54 -0700

    admin changed

--- admin.php
+++ admin.php
@@ -33,20 +33,29 @@ $conn = new mysqli($servername, "cs304", "cs304", "project304");
 			$pass=$_POST["passport"];
 			$pname=$_POST["pname"];
 			$q2 = "SELECT * FROM passengers p, takes t, stops_at s WHERE p.passportNo = '$pass' AND p.name = '$pname' AND t.passportNo = '$pass' AND t.flightNo = s.flightNo";
-			echo $q2;
+			//echo $q2;
 			$r2 = $conn->query($q2);
-			echo $r2->num_rows;
+			//echo $r2->num_rows;
 			while($row = $r2->fetch_assoc()){
 				printf("%s %s %s %s %s %s",$row["passportNo"],$row["name"],$row["specialAssistant"],$row["class"],$row["seatNo"],$row["flightNo"]);
 				$self=htmlspecialchars($_SERVER['PHP_SELF']);
 				echo "<form action='$self' method='post'>
+				<input type='text' value='$pass' id='pass' name='pass' style='display:none'>
 				<textarea rows='4' cols='50' name='sa' id='sa' placeholder='Update Special Assistant'></textarea>
 				<br>
 				<input type='submit' value='Update Special Assistant'>
 				</form>";
 			}
 		} else if(!empty($_POST["sa"])){
-
+			$pass = $_POST["pass"];
+			$s = $_POST["sa"];
+			$q4= "UPDATE takes SET specialAssistant= '$s' where passportNo = '$pass'";
+			//echo $q4;
+			if($conn->query($q4) == true){
+				echo "Record updated successfully";
+			} else {
+				echo "Error updating record: " . $conn->error;
+			}
 		};
 	}
 	?>
