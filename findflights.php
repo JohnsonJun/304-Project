cd8a850f637e4a5c3bdfdae159068fbdbfd14a89
GaohanC <gaohanc@alumni.ubc.ca>
2016-04-03 19:26:50 -0700

    findflight.php

--- findflights.php
+++ findflights.php
@@ -5,20 +5,33 @@ $conn = new mysqli($servername, "cs304", "cs304", "project304");
 // if($_POST["dep"]!=""){
 // 	echo $_POST["dep"];
 // }
+?>
+<!DOCTYPE HTML>
+<html>
+<head>
+	<title>Flight Info
+	</title>
+</head>
+<body style="background:url('https://images.trvl-media.com/media/content/expus/graphics/launch/home/tvly/150324_flights-hero-image_1330x742.jpg')  ">
+<center>
+<h1>Flight Info</h1>
+<?php
 $dep = $_POST["dep"];
 $des = $_POST["arv"];
-if($conn){
-	echo "Connected";
-	echo "<br>";
-}
+
 
 if($_POST["dept"] ==""){
 	$q1 = "SELECT * FROM flights WHERE departureLoc LIKE '$dep' AND destination LIKE '$des'";
 	$r1 = $conn->query($q1);
-	echo $r1->num_rows;
-	echo "<br>";
+
+	
 	while($row = $r1->fetch_assoc()){
-		printf("%s %s %s %d %d %d %s",$row["flightNo"],$row["departureLoc"],$row["destination"], $row["departureTime"],$row["arrivalTime"],$row["gateNo"],$row["companyName"]); 	
+		printf("<div style='border:1px; display:inline-block; font-size:larger'>FlightNo:<br> %s</div>
+			<div style='border:1px; display:inline-block; font-size:larger'>Departure From:<br> %s</div>
+			<div style='border:1px; display:inline-block; font-size:larger'>Destination:<br> %s</div>
+			<div style='border:1px; display:inline-block; font-size:larger'>Company:<br> %s</div><br>
+			<div style = 'font-size:large'>You Will Be Boarding at Gate Number:<div style='border:1px solid black; display:inline-block;'> %d</div></div>"
+			,$row["flightNo"],$row["departureLoc"],$row["destination"],$row["companyName"],$row["gateNo"]); 	
 	}
 } else {
 	$dept = $_POST["dept"];
@@ -28,19 +41,28 @@ if($_POST["dept"] ==""){
 	$r2 = $conn->query($q2);
 	//echo "<br>";
 	//echo "FIND";
-	echo $r2->num_rows;
-	echo "<br>";
+	
+	
 	while($row = $r2->fetch_assoc()){
-		printf("%s %s %s %d %d %d %s",$row["flightNo"],$row["departureLoc"],$row["destination"], $row["departureTime"],$row["arrivalTime"],$row["gateNo"],$row["companyName"]);
+		printf("<div style='border:1px; display:inline-block; font-size:larger'>FlightNo:<br> %s</div>
+			<div style='border:1px; display:inline-block; font-size:larger'>Departure From:<br> %s</div>
+			<div style='border:1px; display:inline-block; font-size:larger'>Destination:<br> %s</div>
+			<div style='border:1px; display:inline-block; font-size:larger'>Company:<br> %s</div><br>
+			<div style = 'font-size:large'>You Will Be Boarding at Gate Number:<div style='border:1px solid black; display:inline-block;'> %d</div></div>"
+			,$row["flightNo"],$row["departureLoc"],$row["destination"],$row["companyName"],$row["gateNo"]); 
 		$deptime = new DateTime($row["departureTime"]);
 		$arvtime = new DateTime($row["arrivalTime"]);
 		echo "<br>";
-		printf("DepTime: ");
+		printf("<div style = 'font-size:large'>DepTime: </div>");
 		printf($deptime->format("Y-m-d H:i:s"));
 		echo "<br>";
-		printf("ArvTime: ");
+		printf("<div style = 'font-size:large'>ArvTime: </div>");
 		printf($arvtime->format("Y-m-d H:i:s"));
 	}
 }
 ?>
+</body>
+</center>
+</html>
+
 
