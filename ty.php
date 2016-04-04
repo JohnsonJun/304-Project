a9e980250c3a5ecaa285489c659a65350b5c3cc2
GaohanC <gaohanc@alumni.ubc.ca>
2016-04-03 17:42:09 -0700

    change the format of findyourflight

--- ty.php
+++ ty.php
@@ -8,7 +8,7 @@ $conn = new mysqli($servername, "cs304", "cs304", "project304");
 	<title>Boading Pass
 	</title>
 </head>
-<body style="background:url('http://www.stillstoked.com/wp-content/uploads/2015/05/The-plane-flying-at-sunset-airliner-photography_1920x1080.jpg') top center no-repeat; ">
+<body style="background:url('https://scstylecaster.files.wordpress.com/2013/12/flight-tracker.jpg') top center no-repeat; ">
 <center>
 <!--h1>Boading Pass</h1-->
 
@@ -22,13 +22,24 @@ echo "<h1>".$pname."'s Boarding Pass</h1>";
 $r1 = $conn->query($q1);
 //echo "<br>";
 while($row = $r1->fetch_assoc()){
-	printf("
-		<div style='border:1px; display:inline-block; font-size:larger'>Class:<br> %s</div> 
-		<div style='border:1px; display:inline-block; font-size:larger'>SeatNo:<br>%s</div>
-		<div style='border:1px; display:inline-block; font-size:larger'>FlightNo:<br> %s</div>
-		<div style='border:1px; display:inline-block; font-size:larger'>DepartureFrom:<br> %s</div>
-		<div style='border:1px; display:inline-block; font-size:larger'>Destination:<br> %s</div>
-		<div style='border:1px; display:inline-block; font-size:larger'>Company:<br> %s</div><br>
+	printf("<table border='5' style='width:100%'>
+		<tr>
+    <td>Class:</td>
+    <td>SeatNo:</td>		
+    <td>Flight No:</td>
+    <td>DepartureFrom:</td>
+    <td>Destination:</td>
+    <td>Company:</td>
+    </tr>
+    <tr>
+    <td>%s</td>
+    <td>%s</td>
+    <td>%s</td>
+    <td>%s</td>
+    <td>%s</td>
+    <td>%s</td>
+    </tr>
+    </table>
 		You Will Be Boarding at Gate Number:<div style='border:1px solid black; display:inline-block;'> %d</div>", $row["class"],$row["seatNo"],$row["flightNo"],$row["departureLoc"],$row["destination"],$row["companyName"],$row["gateNo"]);
 }
 ?>
